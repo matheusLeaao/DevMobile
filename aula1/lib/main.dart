da400820c5ee95d2b7177cd342aac7500fb29b06
LABORATÓRIO 803 <lab803@dominio.anhembimorumbi.edu.br>
2020-03-05 20:18:22 -0300

    Comitando arquivos

---
+++ aula1/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Dart code to show a console message
+void main() => runApp(
+  Column(
+    children: <Widget> [
+      Text("Olá mundo!", textDirection: TextDirection.ltr),
+      Text("Sejam bem-vindos", textDirection: TextDirection.ltr),
+    ]
+  )
+);
