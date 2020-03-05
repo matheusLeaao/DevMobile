37cbb8318eb55b257f124b9bfb34bc33d449f377
LABORATÓRIO 803 <lab803@dominio.anhembimorumbi.edu.br>
2020-03-05 20:43:36 -0300

    Scaffold e AppBar

--- aula1/lib/main.dart
+++ aula1/lib/main.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 
 // Dart code to show a console message
 void main() => runApp(
-  Column(
-    children: <Widget> [
-      Text("Olá mundo!", textDirection: TextDirection.ltr),
-      Text("Sejam bem-vindos", textDirection: TextDirection.ltr),
-    ]
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(),
+    )
   )
 );
