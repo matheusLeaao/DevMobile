28a64493b458db047c9f70d01fb01a1ac7acd0f7
LABORATÓRIO 803 <lab803@dominio.anhembimorumbi.edu.br>
2020-03-05 21:20:24 -0300

    Botões

--- aula1/lib/main.dart
+++ aula1/lib/main.dart
@@ -4,7 +4,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     home: Scaffold(
-      appBar: AppBar(),
-    )
-  )
+      appBar: AppBar(
+        title: Text("Aula 01"),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            tooltip: "Show Snakbar",
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            tooltip: "Account",
+            onPressed: () {},
+          ),  
+        ],
+      ),
+      body: const Image(
+        image: NetworkImage(
+          "https://www.estudokids.com.br/wp-content/uploads/2018/11/galaxia-1200x675.jpg",  
+        ),
+      ),
+    ),
+  ),
 );
